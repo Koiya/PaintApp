29b3a6926639cc72afd3b4232aea6b1467f69edf
Daniel Nguyen <Koiya@users.noreply.github.com>
2023-01-26 15:55:08 -0600

    paintapp js updated

--- PaintApp/sketch.js
+++ PaintApp/sketch.js
@@ -1,7 +1,63 @@
+var boxes = [];
+var currentColor = 'red'
+let colors = [
+  'red',
+  'orange',
+  'yellow',
+  'lime',
+  'cyan',
+  'blue',
+  'magenta',
+  'rgb(92, 64, 51)',
+  'white',
+  'black'
+];
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
+  var posY = 0;
+  for(i = 0; i < colors.length; i++){ 
+    console.log(colors[i])
+    boxes[i] = new colorBox(posY,colors[i]);
+    posY = posY + 20;
+  }
+}
+function draw() {
+  stroke(255);
+  strokeWeight(1);
+  for(i = 0; i < colors.length; i++){
+    boxes[i].draw()
+  }
+  if (mouseIsPressed)
+  {
+  stroke(currentColor)
+  strokeWeight(10)
+  line(pmouseX, pmouseY, mouseX, mouseY);
+  }
 }
 
-function draw() {
-  background(220);
+function mouseClicked(){
+  for(i = 0; i < colors.length; i++){
+    boxes[i].mouseClicked()
+  }
+}
+
+class colorBox{
+  constructor(y,c){
+    this.y = y;
+    this.c = c;
+    
+  }
+  draw(){
+    fill(this.c);
+    rect(0,this.y,20,20);
+  }
+  mouseClicked(){
+  
+  let insideX = mouseX >= 0 && mouseX <= 20;
+  let insideY = mouseY >= this.y && mouseY <= this.y + 20;
+  let inside = insideX && insideY;
+  if(inside){
+    currentColor = this.c;
+  }
+  }
 }
